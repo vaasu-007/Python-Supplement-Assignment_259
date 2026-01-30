729fdd6c437653cbe2f44c591d0dee7c4e8f7758
vaasu-007 <vasanth.k_25am@kgkite.ac.in>
2026-01-30 15:14:39 +0530

    problem_16 solved

--- problem_16.py
+++ problem_16.py
@@ -2,6 +2,10 @@
 # Find and fix the error
 
 numbers = [45, 89, 12, 78, 34]
-numbers.sort()
-second_largest = numbers[-2]
+
+unique_numbers = list(set(numbers))
+unique_numbers.sort()
+
+second_largest = unique_numbers[-2]
 print(f"Second largest: {second_largest}")
+
