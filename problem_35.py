e8ba195b1f52754cdb8edcc8cfa906c39f5017b0
vaasu-007 <vasanth.k_25am@kgkite.ac.in>
2026-01-30 18:03:34 +0530

    problem_35_solved

--- problem_35.py
+++ problem_35.py
@@ -5,6 +5,7 @@ def calculate_percentage(obtained, total):
     percentage = (obtained / total) * 100
     return percentage
 
+
 marks = 45
 total_marks = 50
 result = calculate_percentage(marks, total_marks)
