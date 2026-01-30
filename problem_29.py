498c835d328114b24219f3ab326cde0acdbf5e75
vaasu-007 <vasanth.k_25am@kgkite.ac.in>
2026-01-30 15:43:28 +0530

    problem 29 solved

--- problem_29.py
+++ problem_29.py
@@ -1,10 +1,13 @@
 # Problem 29: Function with default argument
 # Find and fix the error
 
-def add_to_list(item, lst=[]):
+def add_to_list(item, lst=None):
+    if lst is None:
+        lst = []
     lst.append(item)
     return lst
 
-print(add_to_list(1))
-print(add_to_list(2))
-print(add_to_list(3))
+print(add_to_list(1))  
+print(add_to_list(2))  
+print(add_to_list(3))  
+
