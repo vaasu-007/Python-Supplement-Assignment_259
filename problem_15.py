f619de0ea79a0c6ab4b3c8d861e0a007b543300f
vaasu-007 <vasanth.k_25am@kgkite.ac.in>
2026-01-30 15:04:57 +0530

    problem-15 solved

--- problem_15.py
+++ problem_15.py
@@ -5,6 +5,7 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius * 9/5 + 32
     return fahrenheit
 
+
 temp_c = 25
 temp_f = celsius_to_fahrenheit(temp_c)
 print(f"{temp_c}°C = {temp_f}°F")
